15e1e6ff17af63276ddc73708de036133386c033
williamjpriest <liuliu6wp@gmail.com>
2023-09-30 06:58:17 +0800

    changed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function footer() {
   return (
     <>
       <footer className='h-20 flex justify-center p-10 border-top border-2'>
-        <p>Created by William J Priest</p>
+        <p>Janus Games Studio</p>
       </footer>
     </>
   )
