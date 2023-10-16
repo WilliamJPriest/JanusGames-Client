01011612acc4c948a2335d4a4479400660540481
williamjpriest <liuliu6wp@gmail.com>
2023-10-16 10:29:23 -0400

    secret func

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { ToastContainer,toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-// import 'dotenv/config'
 
 export default function hero() {
   const [client,setClient]=useState({
--- src/components/unsubscriber.tsx
+++ src/components/unsubscriber.tsx
@@ -1,4 +1,6 @@
 import {useState} from 'react'
+import { ToastContainer,toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function unsubscriber() {
   const [unSubEmail,setUnSubEmail] = useState("")
@@ -7,14 +9,52 @@ export default function unsubscriber() {
     setUnSubEmail( e.target.value)
   }
 
-  const unSubBTN = () =>{
+  const unSubBTN = async (e: any) => {
+    e.preventDefault();
     console.log(unSubEmail)
-  }
-
+    const validRegex = /^\w+([\.+-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (unSubEmail.match(validRegex)) {
+      
+      try {
+        const requestData = {
+          'email': unSubEmail
+        };
+        
+        const response = await fetch(import.meta.env.REACT_APP_REMOVEEMAILAWSFUNC  , {
+          method: 'POST',
+          body: JSON.stringify(requestData),
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin' :'*',
+          },
+        });
+        if (response.ok) {
+          const responseData = await response.json();
+          toast('Success: ' + responseData.message);
+          console.log(requestData.email)
+          // setClient({
+          //   ...unSubEmail,
+          //   name: "",
+          //   email:""
+          // })
+          
+        } else {
+          const errorData = await response.json();
+          toast('Error: ' + errorData.message);
+        }
+      } catch (error) {
+        toast('Error: ' + "try again later");
+      }
+    } else {
+      toast('Email invalid');
+    }
+    
+  };
   return (
     <>
       <div>
         <h3>Unsubscribe</h3>
+        <ToastContainer/>
         <div>
             <input onChange={handleChange} value={unSubEmail} placeholder="email"/>
             <button onClick={unSubBTN}>Unsubscribe</button>
